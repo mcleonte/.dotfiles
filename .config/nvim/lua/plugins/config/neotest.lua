f9fc25bc740411b01c8d3422a2deaf0350c5bb30
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 04:12:58 +0200

    create on_attach func

--- .config/nvim/lua/plugins/config/neotest.lua
+++ .config/nvim/lua/plugins/config/neotest.lua
@@ -1,5 +1,14 @@
+local set = vim.keymap.set
+
 return function()
-  require("neotest").setup({
+  local neotest = require("neotest")
+
+  local on_attach = function()
+    -- set("n", "<leader>T", neotest.run.run())
+  end
+
+  neotest.setup({
+    on_attach = on_attach,
     adapters = {
       require("neotest-python")({
         dap = { justMyCode = false },
