b519f83d06896ace6cc2b6b20b2329ef4a89d54e
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 04:12:58 +0200

    new layout

--- .config/nvim/lua/plugins/config/lualine.lua
+++ .config/nvim/lua/plugins/config/lualine.lua
@@ -1,7 +1,66 @@
+local _ = {
+	pwd = function()
+		return vim.fn.fnamemodify(vim.fn.getcwd(-1, -1), ":~")
+	end,
+}
+
 return function()
-  require("lualine").setup({
-    options = {
-      component_separators = { left = "|", right = "|" },
-    },
-  })
+	require("lualine").setup({
+		options = {
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+			globalstatus = true,
+		},
+		sections = {
+			lualine_a = { { "mode", upper = false } },
+			lualine_b = { { "branch", icon = "" }, "diff", "diagnostics" },
+			lualine_c = { { "filename", path = 1, file_status = false, shorting_target = 0 } },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { { "filename", path = 1 } },
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {
+			lualine_a = { { "tabs", mode = 1, max_length = vim.o.columns } },
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {
+				{
+					"windows",
+					show_filename_only = true,
+					mode = 2,
+					show_modified_status = true,
+					filetype_names = {
+						NvimTree = "NTree",
+					},
+				},
+			},
+			lualine_y = {},
+			lualine_z = {},
+		},
+		winbar = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+		inactive_winbar = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+		extensions = { { sections = { lualine_b = { "filetype" } }, filetypes = { "NvimTree" } } },
+	})
 end
