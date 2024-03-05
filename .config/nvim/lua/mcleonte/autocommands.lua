e8066aa3a881c2fe474bb689ded0dd76f06851cd
Mihail Costin Leonte <git@mcleonte.com>
2024-03-05 02:10:37 +0200

    add autocommand to default to dotfiles repo if no .git dir detected

--- .config/nvim/lua/mcleonte/autocommands.lua
+++ .config/nvim/lua/mcleonte/autocommands.lua
@@ -18,3 +18,19 @@ c([[
   au BufEnter <buffer> exec "normal! i"
   au BufWinLeave term://* stopinsert
 ]])
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	pattern = "*",
+	callback = function()
+		-- Check for .git directory
+		local f = io.open(".git", "r")
+		if f ~= nil then
+			io.close(f)
+			return
+		end
+
+		-- Set environment variables
+		vim.env.GIT_DIR = vim.env.HOME .. "/.dotfiles/"
+		vim.env.GIT_WORK_TREE = vim.env.HOME .. "/"
+	end,
+})
